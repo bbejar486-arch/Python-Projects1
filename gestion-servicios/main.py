2b2d0b41e6a441421a5bbbed5344b739bc028600
Bruno Bejar <bbejar486@gmail.com>
2026-09-15 10:24:36 -0300

    Add main menu for service management

--- gestion-servicios/main.py
+++ gestion-servicios/main.py
@@ -1,1 +1,26 @@
+import funcion3
 
+def main():
+    opcion = 0
+
+    while opcion != 5:
+        print("----------- MENU -----------")
+        print("1. Cargar servicios")
+        print("2. Mostrar servicios que esten entre i1 e i")
+        print("3. Mostrar servicios activos")
+        print("4. Buscar servicio por Nombre")
+        print("5. Salir")
+
+        opcion = int(input("Ingrese una opcion: "))
+
+        if opcion == 1:
+            funcion3.cargar()
+
+        if opcion == 2:
+            funcion3.importes(funcion3.objetos)
+
+        if opcion == 3:
+            funcion3.servicios(funcion3.objetos)
+
+if __name__ == "__main__":
+    main()
